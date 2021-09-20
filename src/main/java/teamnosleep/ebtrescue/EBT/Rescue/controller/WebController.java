5fa861754091cfffbe77473d1e40739d39e4d664
Anthony Tran-Phan <tonyphan15@gmail.com>
2021-09-19 19:29:03 -0700

    A3 Commit

--- src/main/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebController.java
+++ src/main/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebController.java
@@ -1,8 +1,15 @@
 package teamnosleep.ebtrescue.EBT.Rescue.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
+	
+	// Assignment 3 Step 3
+		@GetMapping("/anthony_t")
+		public String getURL() {
+			return "Anthony's API";
+		}
 
 }
