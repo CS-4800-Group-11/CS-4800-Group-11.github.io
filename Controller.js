1c3788a6ce820c5c6e3947cb2d052c5f0f20d4d5
WhoNew <22082099+18anguyen33@users.noreply.github.com>
2021-09-21 13:00:25 -0700

    Finished Basic HTML http get example

--- Controller.js
+++ Controller.js
@@ -1,7 +1,14 @@
 // Initialize the Module
 var app = angular.module('EBTMap', []);
 // Initialize the Controller to send data to the html file
-app.controller('MainController', ['$scope', function($scope){
-// Simple list with hello world as we send data through the scope
-    $scope.list = ["Hello", "World"]
-}])
+app.controller('MainController', function($scope, $http){
+    // The format should be very similar to the prof's example
+    $http({
+        method:'GET',
+        url: 'https://cs-4800-group-11.github.io/text.json'
+    }).then(function successCallback(response){
+        var people = JSON.parse(JSON.stringify(response)).data.records;
+        $scope.peoples = people
+    }, function errorCallback(response){
+    });
+});
