4dc949504fbb96af4d5f78e10a724854f46bd11d
Shiv Patel <toshiv31@gmail.com>
2021-09-22 10:12:40 -0700

    Update WebController.java

--- src/main/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebController.java
+++ src/main/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebController.java
@@ -16,4 +16,12 @@ public class WebController {
 	public String ashleyURL() {
 		return "Ashley's API";
 	}
+
+	//A4 Method example
+	Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+	log(doc.title());
+	Elements newsHeadlines = doc.select("#mp-itn b a");
+	for (Element headline : newsHeadlines) {
+  		log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
+	}
 }
