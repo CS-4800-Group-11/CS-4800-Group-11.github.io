e1c8558fec34c7a7719f71cdd02b99a9ad735852
Anthony Tran-Phan <tonyphan15@gmail.com>
2021-09-22 11:27:09 -0700

    Assignment 4 Step 2

--- src/main/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebController.java
+++ src/main/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebController.java
@@ -1,7 +1,15 @@
 package teamnosleep.ebtrescue.EBT.Rescue.controller;
 
+import java.io.*;
+import java.net.*;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.maxmind.geoip2.WebServiceClient;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+import com.maxmind.geoip2.record.Location;
 
 @RestController
 public class WebController {
@@ -15,6 +23,25 @@ public class WebController {
 	@GetMapping("/ashley_k")
 	public String ashleyURL() {
 		return "Ashley's API";
+	}
+	
+	// Assignment 4 Step 2
+	@GetMapping("/location")
+	public String geolocation() throws IOException, GeoIp2Exception {		
+		URL whatismyip = new URL("http://checkip.amazonaws.com");
+		BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+		String publicIP = in.readLine();
+		
+		String geoLocation = "";
+		try (WebServiceClient client = new WebServiceClient.Builder(606139, "cjsfbrqhPZj9omqT").host("geolite.info")
+		        .build()) {
+			
+		    InetAddress ipAddress = InetAddress.getByName(publicIP);
+		    CityResponse response = client.city(ipAddress);
+		    Location location = response.getLocation();
+		    geoLocation += (location.getLongitude() + ", " + location.getLatitude());
+		}
+		return geoLocation.equals("") ? "Could not get your location" : geoLocation;
 	}
 
 	//A4 Method example
