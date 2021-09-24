470a1e09bc06b6fef901402b580ec6cdf9910141
Allison Chen <allisonchen506@gmail.com>
2021-09-23 22:51:36 -0700

    A4

--- src/main/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebController.java
+++ src/main/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebController.java
@@ -65,5 +65,13 @@ public class WebController {
 	System.out.println(jsonNode.get("records").get("Name");
 
 	//A4
-
+	double[] values = new double[] {65, 51 , 16, 11 , 6519, 191 ,0 , 98, 19854, 1, 32};
+	DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
+	for (double v : values) {
+		descriptiveStatistics.addValue(v);
+	}
+	
+	double mean = descriptiveStatistics.getMean();
+	double median = descriptiveStatistics.getPercentile(50);
+	double standardDeviation = descriptiveStatistics.getStandardDeviation();
 }
