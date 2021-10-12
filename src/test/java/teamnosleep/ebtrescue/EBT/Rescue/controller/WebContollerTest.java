bebf87ed063996566cbdc1c39b77db850ffdf4db
Ashley Kang <ashleykang483@gmail.com>
2021-10-11 23:46:20 -0700

    A6

--- src/test/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebContollerTest.java
+++ src/test/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebContollerTest.java
@@ -2,16 +2,21 @@ package teamnosleep.ebtrescue.EBT.Rescue.controller;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class WebContollerTest {
 	
-	private WebController webController;
+	@Autowired private WebController webController;
 	
 	@Test
 	public void anthonyApiTest() {
-		webController = new WebController();
 		Assert.assertEquals("Anthony's API", webController.getURL());
+	}
+
+	@Test
+	public void ashleyAPITest() {
+		Assert.assertEquals("Ashley's API", webController.ashleyURL());
 	}
 }
