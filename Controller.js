c8c6ca9ecf86d3012a3c9cd5563b4ee2cf0b4a49
WhoNew <22082099+18anguyen33@users.noreply.github.com>
2021-10-19 11:28:46 -0700

    Accidentally broke something
    Just more testing to make sure it works

--- Controller.js
+++ Controller.js
@@ -1,9 +1,6 @@
 // Combined the two to make data transfer more convenient
 // Angular Section
-var current_location = {
-    lat: 0,
-    lng: 0
-  }
+
 // Initialize the Module
 var app = angular.module('EBTMap', []);
 // Initialize the Controller to send data to the html file
@@ -29,74 +26,15 @@ app.controller('MainController', function($scope, $http){
 
     $http({
         method:'POST',
-        url: '/currentLocation'
+        url: '/currentLocation.txt',
         data: current_location
     }).then(function successCallback(response){
-        $scope.students = response.data;
+        console.log("Send Data: "+current_location.lat + " "+ current_location.lng);
     }, function errorCallback(response){
         console.log("error in sending current location");
     });
 });
 
-// Map Section
-let map;
-function initMap() {
-  var location = {
-    lat: 34.049332,
-    lng: -117.814583
-  }
-  var options = {
-    center: location,
-    zoom: 20
-  }
-  const Science_Building = {lat: 34.058515, lng: -117.824858};
-  if (navigator.geolocation){
-    console.log("Geolocation Supported");
-    navigator.geolocation.getCurrentPosition((loc) => {
-        location.lat = loc.coords.latitude;
-        location.lng = loc.coords.longitude;
-        current_location.lat = loc.coords.latitude;
-        current_location.lng = loc.coords.longitude;
-        map = new google.maps.Map(document.getElementById("map"), options);
-    },
-    (err) => {
-        console.log("User Clicked No");
-        map = new google.maps.Map(document.getElementById("map"), options);
-    }
-    )
-  } else{
-    console.log("Geolocation Not Supported");
-    map = new google.maps.Map(document.getElementById("map"), options);
-  }
-  autocomplete = new google.maps.places.Autocomplete(document.getElementById("search"),
-  {
-    componentRestrictions:{'country':['us']},
-    fields: ['geometry', 'name'],
-    types: ['geocode']
-  });
-
-  autocomplete.addListener("place_changed", () => {
-    const place = autocomplete.getPlace();
-    console.log(place);
-    location.lat = place.geometry.location.lat();
-    location.lng = place.geometry.location.lng();
-    new google.maps.Marker({
-        position: place.geometry.location,
-        title: place.name,
-        map: map
-    })
-    map = new google.maps.Map(document.getElementById("map"), options);
-  });
-
-  var list = [location, Science_Building];
-  var ob = [];
-  for (let i=0;i<list.length;i++){
-    ob[i] = new google.maps.Marker({
-            position: list[i],
-            map: map,
-     });
-  }
-}
 
 
 
---
+++ map.js
@@ -0,0 +1,67 @@
+let map;
+let current_location = {
+    lat: 0,
+    lng: 0
+  }
+function initMap() {
+  var location = {
+    lat: 34.049332,
+    lng: -117.814583
+  }
+  var options = {
+    center: location,
+    zoom: 20
+  }
+  const Science_Building = {lat: 34.058515, lng: -117.824858};
+  if (navigator.geolocation){
+    console.log("Geolocation Supported");
+    navigator.geolocation.getCurrentPosition((loc) => {
+        location.lat = loc.coords.latitude;
+        location.lng = loc.coords.longitude;
+        current_location.lat = loc.coords.latitude;
+        current_location.lng = loc.coords.longitude;
+        console.log(current_location);
+        map = new google.maps.Map(document.getElementById("map"), options);
+    },
+    (err) => {
+        console.log("User Clicked No");
+        map = new google.maps.Map(document.getElementById("map"), options);
+    }
+    )
+  } else{
+    console.log("Geolocation Not Supported");
+    map = new google.maps.Map(document.getElementById("map"), options);
+  }
+  autocomplete = new google.maps.places.Autocomplete(document.getElementById("search"),
+  {
+    componentRestrictions:{'country':['us']},
+    fields: ['geometry', 'name'],
+    types: ['geocode']
+  });
+
+  autocomplete.addListener("place_changed", () => {
+    const place = autocomplete.getPlace();
+    location.lat = place.geometry.location.lat();
+    location.lng = place.geometry.location.lng();
+    current_location.lat = place.geometry.location.lat();
+    current_location.lng = place.geometry.location.lng();
+    new google.maps.Marker({
+        position: place.geometry.location,
+        title: place.name,
+        map: map
+    })
+    map = new google.maps.Map(document.getElementById("map"), options);
+  });
+
+  var list = [location, Science_Building];
+  var ob = [];
+  for (let i=0;i<list.length;i++){
+    ob[i] = new google.maps.Marker({
+            position: list[i],
+            map: map,
+     });
+  }
+}
+
+
+
