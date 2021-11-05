6b9b929ef06414559d2b7e24b5b11b3e2a2486a5
Anthony Tran-Phan <tonyphan15@gmail.com>
2021-11-04 17:49:16 -0700

    Reorganized front end files

--- src/main/java/teamnosleep/ebtrescue/EBT/Rescue/EbtRescueApplication.java
+++ src/main/java/teamnosleep/ebtrescue/EBT/Rescue/EbtRescueApplication.java
@@ -3,6 +3,9 @@ package teamnosleep.ebtrescue.EBT.Rescue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class EbtRescueApplication {
@@ -10,5 +13,5 @@ public class EbtRescueApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EbtRescueApplication.class, args);
 	}
-
+	
 }
--- src/main/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebController.java
+++ src/main/java/teamnosleep/ebtrescue/EBT/Rescue/controller/WebController.java
@@ -1,19 +1,13 @@
 package teamnosleep.ebtrescue.EBT.Rescue.controller;
 
-import java.io.*;
-import java.net.*;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.JsonNode;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-@RestController
+@Controller
 public class WebController {
 	
+	@GetMapping("/")
+	public String home() {
+		return "index.html";
+	}
 }
