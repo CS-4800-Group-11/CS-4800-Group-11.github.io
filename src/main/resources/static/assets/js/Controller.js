5a6eb4f44b0ec0f09028eaa9d19f1d4a0bcd213e
Anthony Tran-Phan <tonyphan15@gmail.com>
2021-11-04 21:21:18 -0700

    Small changes to fix cross origin error

--- src/main/resources/static/assets/js/Controller.js
+++ src/main/resources/static/assets/js/Controller.js
@@ -5,7 +5,6 @@ var distance_amount = 0
 var num_stores = 0
 // Initialize the Module
 var app = angular.module('EBTMap', []);
-var base_address = 'http://ec2-13-52-81-37.us-west-1.compute.amazonaws.com:8080/nearestStores?latitude='
 // Initialize the Controller to send data to the html file
 app.controller('MainController', function($scope, $http){
     window.initFirst = () => {
@@ -13,10 +12,10 @@ app.controller('MainController', function($scope, $http){
         num_stores = document.getElementById("store_number").value
         $http({
             method:'GET',
-            url: 'http://ec2-13-52-81-37.us-west-1.compute.amazonaws.com:8080/nearestStores?latitude='+ current_location.lat +'&longitude='+ current_location.lng +'&distance='+ distance_amount +'&limit='+ num_stores
+            url: 'http://localhost:8080/nearestStores?latitude='+ current_location.lat +'&longitude='+ current_location.lng +'&distance='+ distance_amount +'&limit='+ num_stores
         }).then(function successCallback(response){
-            stores = JSON.parse(JSON.stringify(response)).data.records;
-            print(stores);
+            stores = JSON.parse(JSON.stringify(response)).data;
+            console.log(stores);
         }, function errorCallback(response){
         });
     }
